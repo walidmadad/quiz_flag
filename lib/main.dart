79ffca23e406abbae42feba870627eccbfc38e3d
walidmadad <walid.madad123@gmail.com>
2025-03-18 00:05:44 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +9,94 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text(
+            "Flag Quiz",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+        body: Padding(padding: const EdgeInsets.all(12.0), child: ExamPage()),
+      ),
+    );
+  }
+}
+
+class ExamPage extends StatefulWidget {
+  @override
+  State<ExamPage> createState() => _ExamPageState();
+}
+
+class _ExamPageState extends State<ExamPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 6,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: const AssetImage("images/ar.png"),
+                width: 300,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+
+              SizedBox(height: 10),
+              Text(
+                "Who is the country ?",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: MaterialButton(
+              onPressed: () {},
+              color: Colors.grey,
+              child: Text("Argentine"),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: MaterialButton(
+              onPressed: () {},
+              color: Colors.grey,
+              child: Text("Italy"),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: MaterialButton(
+              onPressed: () {},
+              color: Colors.grey,
+              child: Text("Brazil"),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: MaterialButton(
+              onPressed: () {},
+              color: Colors.grey,
+              child: Text("Urugway"),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
