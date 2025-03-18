2405405d66b4ba5260115c494e5bf622c10ced69
walidmadad <walid.madad123@gmail.com>
2025-03-18 02:44:03 +0100

    add app_brain.dart

---
+++ lib/app_brain.dart
@@ -0,0 +1,14 @@
+import 'Question.dart';
+class AppBrain {
+  List<Question> questions = [
+    Question("Argentine", "ar", true),
+    Question("France", "be", false),
+    Question("Germany", "cm", false),
+    Question("Egypt", "eg", true),
+    Question("Ghana", "gh", true),
+    Question("Hong kong", "hk", true),
+    Question("Italy", "in", false),
+    Question("Vietnam", "ma", false),
+    Question("Sri Lanka", "lk", true),
+  ];
+}
